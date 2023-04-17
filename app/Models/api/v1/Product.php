96215ed4b7d725c7c24f6286a6f3de74bbcd2a74
pseba20 <pseba20@gmail.com>
2023-04-17 02:37:29 +0200

    Observer in product

--- app/Models/api/v1/Product.php
+++ app/Models/api/v1/Product.php
@@ -11,6 +11,7 @@ class Product extends Model
 
     protected $fillable = [
         'name',
+        'code'
     ];
 
     public function images()
--- database/migrations/2023_04_15_224704_create_products_table.php
+++ database/migrations/2023_04_15_224704_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->text('name');
+            $table->text('code')->nullable();
             $table->timestamps();
         });
     }
